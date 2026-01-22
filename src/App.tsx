675d166aa428c05453c792c51754f0217e46f552
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-01-22 15:22:40 +0530

    .env.example set

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useQuery } from "@tanstack/react-query";
+import { fetchBlogs, Blog } from "./api/blogs";
 
 function App() {
-  return <></>;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["blogs"],
+    queryFn: fetchBlogs,
+  });
+
+  if (isLoading) return <p className="flex item-center justi">Loading...</p>;
+  if (error) return <p className="p-4">Something went wrong</p>;
+
+  return (
+    <div className="p-6 max-w-3xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Blogs</h1>
+
+      <div className="space-y-4">
+        {data?.map((blog: Blog) => (
+          <div key={blog.id} className="border rounded-lg p-4 shadow-sm">
+            <h2 className="text-xl font-semibold">{blog.title}</h2>
+            <p className="text-gray-600">{blog.content}</p>
+            <p className="text-sm text-gray-400 mt-2">Author: {blog.author}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/api/blogs.ts
+++ src/api/blogs.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = import.meta.env.VITE_BASE_URL
+;
 
 export type Blog = {
   id: number;
